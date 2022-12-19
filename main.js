719dda187997c15e02c4512a65d9dfa3b6799c2a
Matar Jacob <matar.j@outlook.com>
2022-12-19 15:08:14 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+
+// // Excersize A //
+
+let someName = 'Matar';
+console.log(someName);
+someName = 'Jacob';
+alert(someName);
+
+
+// // Excersize B //
+let number = Math.random();
+let multi = number * 3;
+let calcul = `${number}*3 = ${multi}`;
+console.log(calcul);
+
+// Excersize C //
+let favoriteNumber = 6;
+let favoritestringed = favoriteNumber.toString();
+typeof(favoritestringed);
+console.log(favoritestringed);
+
+// Excersize D
+
+const FAVORITENUMBER = 6;
+const halfOfFavNum = 6/2;
+let michaelsFavorite = 13;
+let subtract = michaelsFavorite - FAVORITENUMBER;
+michaelsFavorite *= 26
+console.log(michaelsFavorite)
+
+// Excersize E
+let jobTitle = 'window cleaner';
+let geographicLocation = 'New York';
+let spouse = 'Miriam';
+let numOfChildren = 3;
+console.log(`You will be a ${jobTitle} in ${geographicLocation}, and married to ${spouse} with ${numOfChildren} kids.`)
+
+// Excersize F
+
+const BIRTHYEAR = 1997;
+let futureYear = 2034;
+let myAge = futureYear-BIRTHYEAR
+alert(`My age in year ${futureYear} could be ${myAge} or ${myAge-1}.`)
+
+
+
+
+
+
+
+
+
+
